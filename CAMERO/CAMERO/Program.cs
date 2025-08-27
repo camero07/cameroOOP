e9452ed5c17b8ebaa9ae5009ef611bbc0df1f989
ACT-STUDENT <ACT-STUDENT@LAB3-PC16>
2025-08-27 14:47:26 +0800

    huhuhu

---
+++ CAMERO/CAMERO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAMERO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the book class
+            Shoe myShoe = new Shoe();
+
+            //Set the properties
+            myShoe.Brand = "FUMA";
+            myShoe.Size = 40;
+
+            //Display the Book Information
+            myShoe.DisplayInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CAMERO/CAMERO/Shoe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAMERO
+{
+    internal class Shoe
+    {
+        //Encapsulation
+        private string brand;
+
+        private int size;
+
+        //Set the properties for the Book class fields
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+        //Method to display book information
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {brand} \nSize: {size}");
+        }
+    }
+}
