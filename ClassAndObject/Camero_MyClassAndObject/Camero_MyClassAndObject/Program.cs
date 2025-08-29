1a4dfde9b818b34fa6bf4adb4599be59be2d99eb
camero07 <maryjoycecamero@gmail.com>
2025-08-29 14:48:27 +0800

    EXERCISE2

---
+++ ClassAndObject/Camero_MyClassAndObject/Camero_MyClassAndObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camero_MyClassAndObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiation an object
+            Vehicle vehicle1 = new Vehicle();
+            Vehicle vehicle2 = new Vehicle();
+
+            //Initialize the value of fields
+            vehicle1.brand = "Honda";
+            vehicle1.color = "Black";
+            vehicle1.speed = "High";
+
+            vehicle2.brand = "Ford";
+            vehicle2.color = "Gray";
+            vehicle2.speed = "High";
+
+            //call the method to display information
+            Console.WriteLine("The fastest vehicle in history");
+            vehicle1.DisplayInfo();
+            vehicle2.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassAndObject/Camero_MyClassAndObject/Camero_MyClassAndObject/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camero_MyClassAndObject
+{
+    internal class Vehicle
+    {
+        public string brand;
+        public string color;
+        public string speed;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("\nBRAND: " + brand + "\n" + "COLOR: " + color + "\n" + "SPEED: " + speed);
+        }
+    }
+}
