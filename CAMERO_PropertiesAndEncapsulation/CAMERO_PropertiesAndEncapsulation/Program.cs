d412fb678c4004d9a0bff22f28a1943257e00c05
camero07 <maryjoycecamero@gmail.com>
2025-08-29 14:42:55 +0800

    jykuyi

---
+++ CAMERO_PropertiesAndEncapsulation/CAMERO_PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAMERO_PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        //Mary Joyce E. Camero
+        //IT306
+        //EXER_04: Basics on Encapsulation
+        static void Main(string[] args)
+        {
+            //Instantiate the book class
+            Shoe myShoe = new Shoe();
+
+            //Set the properties
+            myShoe.Brand = "FUMA";
+            myShoe.Size = 40;
+
+            //Display the Book Information
+            myShoe.DisplayInfo();
+
+            Console.ReadKey();
+
+                
+        }
+    }
+}
